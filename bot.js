2ab85f157dfcb3c9dd3e25d84870d417b8557f06
Syntaxbotdiscord <40664614+Syntaxbotdiscord@users.noreply.github.com>
2018-06-28 20:25:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -282,7 +282,7 @@ client.on('message', function(message) {
 
 client.on('message', message => {
   if (true) {
-if (message.content === '---invite') {
+if (message.content === '--invite') {
       message.author.send('  https://discordapp.com/api/oauth2/authorize?client_id=445936658320326656&permissions=8&scope=bot  |  تفضل رابط البوت     ').catch(e => console.log(e.stack));
 
     }
@@ -291,7 +291,7 @@ if (message.content === '---invite') {
 
 
 client.on('message', message => {
-     if (message.content === "---invite") {
+     if (message.content === "--invite") {
      let embed = new Discord.RichEmbed()
   .setAuthor(message.author.username)
   .setColor("#9B59B6")
