6536d3330a85aba8a2182daca7f3385a2317e092
Syntaxbotdiscord <40664614+Syntaxbotdiscord@users.noreply.github.com>
2018-06-28 20:11:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,8 +15,21 @@ client.on('message', message => {
 client.login(process.env.BOT_TOKEN);
 
 
+// say script
+var prefix = "--";
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
 
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
 
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "say") {
+   message.channel.sendMessage(args.join("  "))
+  }
+});
 
 
 
