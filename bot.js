98462a7980fdbfe751fed80d54403113a5706b82
Syntaxbotdiscord <40664614+Syntaxbotdiscord@users.noreply.github.com>
2018-07-04 20:48:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -751,3 +751,16 @@ client.on('message', function(message) {
 });
 
 
+
+
+// Create an event listener for new guild members
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find('name', 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
+});
+
+// Log our bot in
