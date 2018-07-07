586733a7e4114a119a401f81d600f9c851141f05
Syntaxbotdiscord <40664614+Syntaxbotdiscord@users.noreply.github.com>
2018-07-07 02:08:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,6 +22,7 @@ client.login(process.env.BOT_TOKEN);
 //
 //
 // say script
+// say script
 var prefix = "--";
 client.on('message', message => {
   if (message.author.bot) return;
@@ -562,7 +563,7 @@ Channels📚 ${client.channels.size} `)
 
 
 
-.
+
 
 
 
@@ -607,6 +608,59 @@ client.on("message", message => {
 
 
 
+client.on('message', function(message) {
+	const myID = "259750207321931777";
+    let args = message.content.split(" ").slice(1).join(" ");
+    if(message.content.startsWith(prefix + "setname")) {
+		        if(message.author.id !== myID) return;
+            if(!args) return message.reply('اكتب الحالة اللي تريدها.');
+        client.user.setUsername(args);
+        message.channel.send(':white_check_mark: Done!').then(msg => {
+           msg.delete(5000);
+          message.delete(5000);
+        });
+    } else if(message.content.startsWith(prefix + "stream")) {
+		        if(message.author.id !== myID) return;
+            if(!args) return message.reply('اكتب الحالة اللي تريدها.');
+        client.user.setGame(args , 'https://twitch.tv/6xlez1');
+        message.channel.send(':white_check_mark: Done!').then(msg => {
+           msg.delete(5000);
+          message.delete(5000);
+        });
+    } else if(message.content.startsWith(prefix + "play")) {
+				        if(message.author.id !== myID) return;
+            if(!args) return message.reply('اكتب الحالة اللي تريدها.');
+        client.user.setGame(args);
+        message.channel.send(':white_check_mark: Done!').then(msg => {
+           msg.delete(5000);
+          message.delete(5000);
+        });
+    } else if(message.content.startsWith(prefix + "listen")) {
+				        if(message.author.id !== myID) return;
+            if(!args) return message.reply('اكتب الحالة اللي تريدها.');
+        client.user.setActivity(args, {type:'LISTENING'});
+        message.channel.send(':white_check_mark: Done!').then(msg => {
+           msg.delete(5000);
+          message.delete(5000);
+        });
+    } else if(message.content.startsWith(prefix + "watch")) {
+				        if(message.author.id !== myID) return;
+            if(!args) return message.reply('اكتب الحالة اللي تريدها.');
+        client.user.setActivity(args, {type:'WATCHING'});
+        message.channel.send(':white_check_mark: Done!').then(msg => {
+           msg.delete(5000);
+          message.delete(5000);
+        });
+    } else if(message.content.startsWith(prefix + "setavatar")) {
+				        if(message.author.id !== myID) return;
+        client.user.setAvatar(args);
+        message.channel.send(':white_check_mark: Done!').then(msg => {
+                if(!args) return message.reply('اكتب الحالة اللي تريدها.');
+           msg.delete(5000);
+          message.delete(5000);
+        });
+    }
+});
 
 
 
@@ -669,13 +723,124 @@ client.on('message', Codes => {
 
 
 
+	client.on('message', async msg => {
+	var prefix = "--";
+	var user = msg.author;
+			var a = msg.guild.roles.find("name", 'Agar');
+		if(!a){
+        a = await msg.guild.createRole({
+		  name: "Agar",
+          color: "#ffffff",
+          permissions:[]
+		})
+		
+        }
+	    var m = msg.guild.roles.find("name", 'Minecraft');
+	if(!m){
+        m =  await msg.guild.createRole({
+		  name: "Minecraft",
+          color: "#ffffff",
+          permissions:[]
+		})
+        }
+        var f = msg.guild.roles.find("name", 'Fortnite');
+		if(!f){
+        f =  await msg.guild.createRole({
+		  name: "Fortnite",
+          color: "#ffffff",
+          permissions:[]
+		})
+        }
+        var b = msg.guild.roles.find("name", 'Brawlhalla');
+		if(!b){
+        b =  await msg.guild.createRole({
+		  name: "Brawlhalla",
+          color: "#ffffff",
+          permissions:[]
+		})
+        }
+        var black = msg.guild.roles.find("name", 'Blacksquad');
+	if(!black){
+        black =  await msg.guild.createRole({
+		  name: "Blacksquad",
+          color: "#ffffff",
+          permissions:[]
+		})
+        }
+
+		if (msg.content.startsWith(prefix +'addmerole')) {
+
+		if(!msg.channel.guild) return msg.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+msg.channel.send(`يرجي اختيار رتبة اللعبة التي تريدها \n1- لعبة اقاريو ⚽ \n2- لعبة ماين كرافت 👶 \n3- لعبة فورت نايت 👊 \n4- لعبة براوهلا  👌 \n5- لعبة بلاك سكواد 🍸\n6- الغاء ❌ \n7- **لديك60 ثانية للاختيار **\n<@${msg.author.id}>`).then(res => {     
+     res.react('⚽').then(r=>{     
+     res.react('👶').then(r=>{
+     res.react('👊').then(r=>{
+     res.react('👌').then(r=>{
+     res.react('🍸').then(r=>{
+     res.react('❌').then(r=>{
+
+    let aaa = (reaction, user) => reaction.emoji.name === '⚽' && user.id === msg.author.id;    
+    let mmm = (reaction, user) => reaction.emoji.name === '👶' && user.id === msg.author.id;
+    let fff = (reaction, user) => reaction.emoji.name === '👊' && user.id === msg.author.id;
+    let bbb = (reaction, user) => reaction.emoji.name === '👌' && user.id === msg.author.id;
+    let bbbb = (reaction, user) => reaction.emoji.name === '🍸' && user.id === msg.author.id;
+    let ccc = (reaction, user) => reaction.emoji.name === '❌' && user.id === msg.author.id;
+
+    let aa = res.createReactionCollector(aaa, { maxMatches:1 , time: 20000 , });
+    let mm = res.createReactionCollector(mmm, { maxMatches:1 , time: 20000 , });
+    let ff = res.createReactionCollector(fff, { maxMatches:1 , time: 20000 , });
+    let bb = res.createReactionCollector(bbb, { maxMatches:1 , time: 20000 , });
+    let bl = res.createReactionCollector(bbbb,{ maxMatches:1 , time: 20000 , });
+    let cc = res.createReactionCollector(ccc, { maxMatches:1 , time: 20000 , });
+
+aa.on("collect", r => {
+    msg.guild.member(user.id).addRole(a);
+	msg.channel.send('`تم اعطائك رتبة للعبة Agar`');
+	msg.delete();
+	})
+mm.on("collect", r => {
+    msg.guild.member(user.id).addRole(m);
+	msg.channel.send('`تم اعطائك رتبة للعبة Mincraft `');
+	msg.delete();
+})
+ff.on("collect", r => {
+    msg.guild.member(user.id).addRole(f);
+	msg.channel.send('`تم اعطائك رتبة للعبة Fortnite `');
+	msg.delete();
+})
+bb.on("collect", r => {
+    msg.guild.member(user.id).addRole(b);
+	msg.channel.send('`تم اعطائك رتبة للعبة Brawlhalla `');
+	msg.delete();
+})
+bl.on("collect", r => {
+    msg.guild.member(user.id).addRole(black);
+	msg.channel.send('`تم اعطائك رتبة للعبة Blacksquad `');
+	msg.delete();
+})
+cc.on("collect", r => {
+	msg.delete();
+})
+	 })
+	 })
+	 })
+	 })
+	 })
+	 })
+	 })
+	 }
+	 });
+
+
+
+
 
 client.on("message", message => {
   if (message.author.bot) return;
  
   let command = message.content.split(" ")[0];
  
-  if (command === "--mute") {
+  if (command === "$$mute") {
         if (!message.member.hasPermission('MANAGE_ROLES')) return message.reply("** لا يوجد لديك برمشن 'Manage Roles' **").catch(console.error);
   let user = message.mentions.users.first();
   let modlog = client.channels.find('name', 'mute-log');
@@ -703,37 +868,6 @@ client.on("message", message => {
 };
  
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
