402bc1c3fa4fab6e244a5fb872bf9a1ee8691dac
Syntaxbotdiscord <40664614+Syntaxbotdiscord@users.noreply.github.com>
2018-07-07 04:01:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -321,15 +321,12 @@ client.on('message', message => {
 .addField('     **--invite** ' , '**لعمل انفايت للبوت**')
 .addField('     **--bot** ' , '**لمعرفه معلومات عن البوت**')
 .addField('     **--hit** ' , '**لضرب شخص**')
-.addField('     **--cat** ' , '**ل اظهار صور قطط**')
 .addField('     **--rp** ' , '**ل اظهار صور ارانب**')
 .addField('     ** ** ' ,' ** أوآمر الإدآرة ** ')
 .addField('     **--kick ** ' ,' ** للطرد  ** ')
 .addField('     **--ban** ' , '**لـ اعطاء بان للعضو** ')
 .addField('     **--bc ** ' ,' ** للبرودكاست ** ')
 .addField('     **--clear** ' , '**لـ مسح الشات** ')
-.addField('     **--mute** ' , '**لمنع العضو من الكتابه لازم يكون فى رتبه muted**')
-.addField('     **--unmute** ' , '**لجعل العضو قادر على الكتابه**')
 .addField('للأستفسار أو الرد على أسئلتكم وأفكـــاركم كلمني على الديسكورد ' , '**Mas-Mrzizx#9302**')
 .addField('**لدعوة البوت للسيرفر ..**' , '**https://discordapp.com/api/oauth2/authorize?client_id=445936658320326656&permissions=8&scope=bot**')
 .setColor('RANDOM')
@@ -907,35 +904,6 @@ client.on('message', async message => {
 
 
 
-
-
-
-
-
-client.on('message',function(message) {
-    let messageArray = message.content.split(' ');
-    let muteRole = message.guild.roles.get('Muted') || message.guild.roles.find('name', 'Muted');
-    let muteMember = message.mentions.members.first();
-    let muteReason = messageArray[2];
-    let muteDuration = messageArray[3];
-   if(message.content.startsWith(prefix + "mutevoice")) {
-       if(!muteRole) return message.guild.createRole({name: 'Muted'}).then(message.guild.channels.forEach(chan => chan.overwritePermissions(muteRole, {SEND_MESSAGES:false,ADD_REACTIONS:false})));
-       if(!message.guild.member(message.author).hasPermission("MANAGE_ROLES")) return message.channel.send('ℹ **Error:** ``خصائص مفقودة``');
-       if(!message.guild.member(client.user).hasPermission("MANAGE_ROLES")) return message.channel.send('ℹ **Error:** ``خصائص مفقودة مني``');
-       if(!muteMember) return message.channel.send('ℹ **Error:** ``منشن شخص``');
-       if(!muteReason) return message.channel.send('ℹ **Error:** ``حدد سباّ``');
-       if(!muteDuration) return message.channel.send('ℹ **Error:** ``حدد وقت زمني``');
-       if(!muteDuration.match(/[1-7][s,m,h,d,w]/g)) return message.channel.send('ℹ **Error:** ``حدد وقت زمني صحيح``');
-       message.channel.send(`:white_check_mark: **تم اعطاء العضو ميوت : ${muteMember}**`);
-       muteMember.addRole(muteRole);
-       muteMember.setMute(true)
-       .then(() => { setTimeout(() => {
-           muteMember.removeRole(muteRole)
-           muteMember.setMute(false)
-       }, mmss(muteDuration));
-       });
-   } 
-});
 
 
 
