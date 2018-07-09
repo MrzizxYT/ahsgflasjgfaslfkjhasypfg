ec763f0b16a2c1b2d4c73ee122a153b06f8891e3
Syntaxbotdiscord <40664614+Syntaxbotdiscord@users.noreply.github.com>
2018-07-09 04:28:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -606,43 +606,7 @@ client.on('guildMemberAdd', member => {
 // Log our bot in
 
 
-let rebel;
-client.on("ready", async  => {
-    let guild = client.guilds.get("463823539045400588");
-  let users = guild.members.map(member => member.user.id);
-  let i;
-  rebel=0;
-for (i=0 ; i < users.length ; i++) {
- let   check = guild.members.get(users[i]);
-if(!check.voiceChannelID){
-        continue;
-}else{
-  rebel++;
-}
-}
-guild.channels.find('id', '465682557187653644').setName(" Voice「"+rebel+"」");
-  client.setInterval(() =>{
-    let d = Date.now()
-  }, 5000);
-});
-client.on('voiceStateUpdate', (oldMember, newMember) => {
-    let guild = client.guilds.get("463823539045400588");
-let newUserChannel = newMember.voiceChannel
-let oldUserChannel = oldMember.voiceChannel
- if(oldUserChannel === undefined && newUserChannel !== undefined) {
-   rebel++;
-guild.channels.find('id', '465682557187653644').setName(" Voice「"+rebel+"」");
-} else if(newUserChannel === undefined){
-  rebel--;
-guild.channels.find('id', '465682557187653644').setName(" Voice「"+rebel+"」");
-}
-});
-client.on('message', Codes => {
-  
-  if(Codes.content === "-صوت") {
-      Codes.channel.send(" Voice「"+rebel+"」");
-}
-});
+
 
 
 
